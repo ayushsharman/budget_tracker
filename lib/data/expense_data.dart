82ff5fe04ee071c679e5d51f5bc1370e4048dd29
Ayush Sharma <ashsharman123@gmail.com>
2023-09-17 08:58:33 +0530

    adding dataS

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,52 @@
+import 'package:budget_tracker_application/model/expense_item.dart';
+
+class ExpenseData {
+  List<ExpenseItem> overallExpenseList = [];
+
+  List<ExpenseItem> getallExpenseList() {
+    return overallExpenseList;
+  }
+
+  void addNewExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+  }
+
+  void deleteExpense(ExpenseItem newExpense) {
+    overallExpenseList.remove(newExpense);
+  }
+
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thurs';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+    }
+  }
+
+  DateTime startofWeekDate() {
+    DateTime? startofWeek;
+
+    DateTime today = DateTime.now();
+
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Sun') {
+        startofWeek = today.subtract(Duration(days: i));
+      }
+    }
+
+    return startofWeek!;
+  }
+}
