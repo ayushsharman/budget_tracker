afe9d5b864749cd2af341c656e4394ae2e3bb753
Ayush Sharma <ashsharman123@gmail.com>
2023-10-04 20:40:11 +0530

    colors fixation

--- lib/bargraph/bar_graph.dart
+++ lib/bargraph/bar_graph.dart
@@ -45,6 +45,13 @@ class MyBarGraph extends StatelessWidget {
             sideTitles: SideTitles(showTitles: false),
           ),
           bottomTitles: AxisTitles(
+            sideTitles:
+                SideTitles(showTitles: true, getTitlesWidget: getBottomTitles),
+          ),
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(showTitles: false),
+          ),
+          rightTitles: AxisTitles(
             sideTitles: SideTitles(showTitles: false),
           ),
         ),
@@ -74,4 +81,62 @@ class MyBarGraph extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget getBottomTitles(double value, TitleMeta meta) {
+  const style = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.bold,
+    fontSize: 12,
+  );
+
+  Widget text;
+  switch (value.toInt()) {
+    case 0:
+      text = const Text(
+        'Sun',
+        style: style,
+      );
+      break;
+    case 1:
+      text = const Text(
+        'Mon',
+        style: style,
+      );
+      break;
+    case 2:
+      text = const Text(
+        'Tue',
+        style: style,
+      );
+      break;
+    case 3:
+      text = const Text(
+        'Wed',
+        style: style,
+      );
+      break;
+    case 4:
+      text = const Text(
+        'Thur',
+        style: style,
+      );
+      break;
+    case 5:
+      text = const Text(
+        'Fri',
+        style: style,
+      );
+      break;
+    case 6:
+      text = const Text(
+        'Sat',
+        style: style,
+      );
+      break;
+    default:
+      text = const Text('');
+      break;
+  }
+  return SideTitleWidget(child: text, axisSide: meta.axisSide);
 }
