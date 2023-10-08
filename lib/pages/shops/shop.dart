f46981b1647f91db4f47cae84cc23111ac827bca
Ayush Sharma <ashsharman123@gmail.com>
2023-10-08 10:44:16 +0530

    shops page

--- lib/pages/shops/shop.dart
+++ lib/pages/shops/shop.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 class ShopsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Shops Page'),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text("Shops"),
+      ),
+      body: Center(
+        child: Text('Shops Page'),
+      ),
     );
   }
 }
---
+++ lib/pages/shops/shop_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShopCard extends StatelessWidget {
+  const ShopCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
