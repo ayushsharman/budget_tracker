8d5a509397fb61ffa8b075bd1a5eba88a68e8a8a
Ayush Sharma <ashsharman123@gmail.com>
2023-10-11 08:14:28 +0530

    added shops card

--- lib/pages/shops/shop_details_page.dart
+++ lib/pages/shops/shop_details_page.dart
@@ -16,8 +16,35 @@ class ShopDetailsPage extends StatelessWidget {
         body: Column(
           children: [
             Container(
-              height: MediaQuery.of(context).size.height * .4,
-              color: Colors.white,
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey, // Shadow color
+                    offset: Offset(0, 2), // Shadow offset
+                    blurRadius: 4, // Shadow blur radius
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
+                  child: Image.asset(
+                    'asset/shop.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             )
           ],
         ));
