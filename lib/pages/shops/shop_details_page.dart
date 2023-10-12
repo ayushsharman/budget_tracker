26cab6f08473951cac0105542d6f7180a38a34e3
Ayush Sharma <ashsharman123@gmail.com>
2023-10-12 12:01:21 +0530

    added name tile

--- lib/pages/shops/shop_details_page.dart
+++ lib/pages/shops/shop_details_page.dart
@@ -6,47 +6,72 @@ class ShopDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          title: const Text("Shops Details"),
-        ),
-        body: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.4,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text("Shops Details"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30.0),
+                bottomRight: Radius.circular(30.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey, // Shadow color
+                  offset: Offset(0, 2), // Shadow offset
+                  blurRadius: 4, // Shadow blur radius
+                ),
+              ],
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
                   bottomLeft: Radius.circular(30.0),
                   bottomRight: Radius.circular(30.0),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey, // Shadow color
-                    offset: Offset(0, 2), // Shadow offset
-                    blurRadius: 4, // Shadow blur radius
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(30.0),
-                    bottomRight: Radius.circular(30.0),
-                  ),
-                  child: Image.asset(
-                    'asset/shop.png',
-                    fit: BoxFit.cover,
-                  ),
+                child: Image.asset(
+                  'asset/shop.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Black and White Print",
+                    ),
+                    Text(
+                      "Rs. 2",
+                    ),
+                    // Rate Column
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
