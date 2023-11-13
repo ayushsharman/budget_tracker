45d397424dc5d773efe21d73a8bc03dc859adc57
Ayush Sharma <ashsharman123@gmail.com>
2023-11-13 15:00:12 +0530

    get data function

--- lib/data/hive_database.dart
+++ lib/data/hive_database.dart
@@ -19,4 +19,25 @@ class HiveDataBase {
 
     _myBox.put("ALL_EXPENSES", allExpensesFormatted);
   }
+
+  List<ExpenseItem> readData() {
+    List savedExpenses = _myBox.get("ALL_EXPENSES") ?? [];
+    List<ExpenseItem> allExpenses = [];
+
+    for (int i = 0; i < savedExpenses.length; i++) {
+      String name = savedExpenses[i][0];
+      String amount = savedExpenses[i][1];
+      DateTime dateTime = savedExpenses[i][2];
+
+      ExpenseItem expense = ExpenseItem(
+        name: name,
+        amount: amount,
+        dateTime: dateTime,
+      );
+
+      allExpenses.add(expense);
+    }
+
+    return allExpenses;
+  }
 }
